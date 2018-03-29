1e1be5f7bb94a0d7a9672f36ca5fd2abedc81eb3
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-03-30 09:53:29 +1000

    Adding basic page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,10 +68,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Basics</a>
+                    <a class="nav-link" href="/basics">Basics</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Raags</a>
