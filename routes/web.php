6ee2533ea2adbeb0348efb4c029f595de91d304e
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-03-30 09:54:25 +1000

    Adding basics route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/basics', function() {
+    return view('basics');
+});
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
