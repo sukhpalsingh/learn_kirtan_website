97a9a59ac177721d20608259a33c601e8b371684
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-04-16 20:37:25 +1000

    Adding basics sidenav and gurbani fonts

--- resources/views/basics.blade.php
+++ resources/views/basics.blade.php
@@ -9,23 +9,32 @@
     <div class="card-body text-center">
 
         <h4 class="card-title"><strong>Kirtan Basics</strong></h4>
+        <div class="row">
+            <div class="col-3">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-intro" aria-selected="true">Introduction</a>
+                    <a class="nav-link" id="v-pills-sargams-tab" data-toggle="pill" href="#v-pills-sargams" role="tab" aria-controls="v-pills-sargams" aria-selected="false">Sargams</a>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-intro-tab">
+                        <p>
+                            <blockquote class="blockquote gurbani-font-1">
+                                kIrqn is~Kx leI sB qoN mh~qvpUrn hY gurU swihb dI ndir Aqy ikrpw[ pRmwqmw ijs au~pr qruT jwvy aus nUM kIrqn krn dI dwq pRwpq huMdI hY[
+                            </blockquote>
+                        </p>
 
-        <h5 class="indigo-text"><strong>Sargams</strong></h5>
-        <p class="card-text">
-            Sevet set of notes id called 'Sargam' but in playing a sargam, 'Sa' of next scale is added, thus making eight notes.
-            It is important to learn the sargam keys thoroughly; This will make the playing of the harmonium very easy.
-        </p>
-        <img src="build/images/sargam.jpg" class="img-fluid" />
-
-        <h5 class="indigo-text">Types of Scales</h5>
-        <p class="card-text">
-            There are three types of the scales in harmonium:
-            <ol>
-                <li>Lower Sclae</li>
-                <li>Middle Scale</li>
-                <li>Upper Scale</li>
-            </ol>
-        </p>
+                        <p class="card-text gurbani-font-1">
+                                
+                        </p>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-sargams" role="tabpanel" aria-labelledby="v-pills-sargams-tab">
+                        Sargams
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
