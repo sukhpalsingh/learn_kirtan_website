c7642c4c4c53c97b70a37377846ca1e115c09eb4
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-04-18 07:07:07 +1000

    Adding sargams section

--- resources/views/basics.blade.php
+++ resources/views/basics.blade.php
@@ -53,13 +53,41 @@
             <div class="col-sm-9">
                 <div class="tab-content" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-intro-tab">
-                        <p>
-                            <blockquote class="blockquote gurbani-font-2">
-                                kIrqn is~Kx leI sB qoN mh~qvpUrn hY gurU swihb dI ndir Aqy ikrpw[ pRmwqmw ijs au~pr qruT jwvy aus nUM kIrqn krn dI dwq pRwpq huMdI hY[
-                            </blockquote>
-                        </p>
+                        <div class="row">
+                            <div class="col-md-12 gurbani-font-2">
+                                <p>
+                                    <blockquote class="blockquote">
+                                        kIrqn is~Kx leI sB qoN mh~qvpUrn hY gurU swihb dI ndir Aqy ikrpw[ pRmwqmw ijs au~pr qruT jwvy aus nUM kIrqn krn dI dwq pRwpq huMdI hY[
+                                    </blockquote>
+                                </p>
 
-                        <img src="/build/images/guru_nanak_dev_ji.png" />
+                                <img src="/build/images/guru_nanak_dev_ji.png" />
+
+                                <p class="mb-1 font-size-20 mt-4">
+                                    is~K sMgIq jW sbd kIrqn sRI gurU gRMQ swihb qoN Sbd dw gwien hY, jo ik is~K Drm dw mu~K AMg hY[
+                                </p>
+
+                                <p class="mb-1 font-size-20">
+                                    ieh prMprw 16 vIN sdI dy AKIr iv~c is~KW dy sMsQwpk gurU nwnk dyv jI dy pihly AnuBvI BweI mrdwnw jI duAwrw sMgIiqk swj rbwb qoN SurU kIqI sI[
+                                </p>
+
+                                <p class="mb-1 font-size-20">
+                                    ies kIrqn nUM gurU nwnk dyv jI, gurU AMgd dyv jI, gurU Amrdws jI, gurU rwmdws jI, gurU Arjn dyv jI, gurU hrgoibMd swihb jI, gurU qyg bhwdr swihb jI Aqy gurU goibMd isMG jI ny gwieAw Aqy Awpxy is~KW nUM vI kIrqn gwaux Aqy sunx dw aupdys id~qw[
+                                </p>
+
+                                <p class="mb-1 font-size-20">
+                                    prucilq klwsIkl Aqy lok sMgIq, qMqI swjW kIrqn bhuq mh~qv vwlw sI Aqy gurmiq sMgIq ny mh~qvpUrx BUimkw inBwauNdy hoey BgqI nUM Aqy sMgIq jgq nUM ie~k Adu~qI rsqw idKwieAw[
+                                </p>
+
+                                <p class="mb-1 font-size-20">
+                                    gurU swihbW ny gurU gRMQ swihb ivc hr Sbd leI rwg drswieAw, ijs nwl ieh prMprw ie~k vfmu~lI dwq hY[
+                                </p>
+
+                                <p class="mb-1 font-size-20">
+                                    is~K gurUAW duAwrw SurU Aqy isq kIqy AmIr is~K sMgIq prMprw nUM muV surjIq krn leI 1970 dy dhwky qoN bhuq mh~qvpUrn koiSSW ho rhIAW hn[
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div
                         class="tab-pane fade"
@@ -67,18 +95,20 @@
                         role="tabpanel"
                         aria-labelledby="v-pills-sargams-tab"
                     >
-                        <p class="gurbani-font-2 justify-content-between">
-                            ‘sw’ (gwien dw pihlw sur jwN mUl sur) gwien dw ADwr hY, ieh jwxn leI sB qoN pihlwN swnUM Awpxw skyl pqw hoxw cwhIdw hY[
-                        </p>
-                        <p class="gurbani-font-2 justify-content-between">
-                            iksy vI gwien dy iqMn ih~sy huMdy hn[ kIrqn krn leI Awpxw skyl cux ky iqMn spqkwN dI jwxkwrI hoxI cwhIdI hY[ iehnwN spqkwN qy gwien krn leI srgmwN qwnwN dw AiBAws krnw cwhIdw hY[
-                        </p>
+                        <div class="row">
+                            <p class="col-sm-12 gurbani-font-2 justify-content-between font-size-20">
+                                ‘sw’ (gwien dw pihlw sur jwN mUl sur) gwien dw ADwr hY, ieh jwxn leI sB qoN pihlwN swnUM Awpxw skyl pqw hoxw cwhIdw hY[
+                            </p>
+                            <p class="col-sm-12 gurbani-font-2 justify-content-between font-size-20">
+                                iksy vI gwien dy iqMn ih~sy huMdy hn[ kIrqn krn leI Awpxw skyl cux ky iqMn spqkwN dI jwxkwrI hoxI cwhIdI hY[ iehnwN spqkwN qy gwien krn leI srgmwN qwnwN dw AiBAws krnw cwhIdw hY[
+                            </p>
+                        </div>
                         <ul class="list-group">
                             <li class="list-group-item gurbani-font-2">
-                                <div class="d-flex w-100 justify-content-between">
+                                <div class="d-flex w-100 justify-content-between font-size-20">
                                     <h4 class="mb-1">1. mMdr spqk</h4>
                                 </div>
-                                <p class="mb-1">
+                                <p class="mb-1 font-size-20">
                                     ies spqk qy gwien krnw kuJ muSikl hY ies leI kw&I AiBAws krnw pYNdw hY pr ies spqk qy gwien krn nwl gwien iv~c bhuq Tihrwau AwauNdw hY[
                                 </p>
                             </li>
@@ -86,7 +116,7 @@
                                 <div class="d-flex w-100 justify-content-between">
                                     <h4 class="mb-1">2. m~D spqk</h4>
                                 </div>
-                                <p class="mb-1">
+                                <p class="mb-1 font-size-20">
                                     m~D spqk qy gwien krnw Awswn hY ikauNik ies spqk qy gwien krn leI ijAwdw AiBAws dI loV nhIN huMdI, ieh AwswnI nwl pkV ivc Aw jwNdw hY ikauNik ies spqk dw quhwfy gly dI sur nwl jldI myl bx jwNdw hY[
                                 </p>
                             </li>
@@ -94,18 +124,58 @@
                                 <div class="d-flex w-100 justify-content-between">
                                     <h4 class="mb-1">3. qwr spqk</h4>
                                 </div>
-                                <p class="mb-1">
+                                <p class="mb-1 font-size-20">
                                     qwr spqk mqlb au~cy surwN dw spqk[ ies p~Dr qy gwien krn leI vI bhuq AiBAws dI loV huMdI hY Aqy zordwr AiBAws krnw cwhIdw hY, ijs nwl spqk qy gwien krnw Awswn huMdw hY Aqy gwien krn sunx iv~c AwnMd Awaudw hY[
                                 </p>
                             </li>
                         </ul>
                     </div>
                     <div class="tab-pane fade" id="v-pills-harmonium" role="tabpanel" aria-labelledby="v-pills-harmonium-tab">
-                        <img src="build/images/men_kirtan_layout.png" class="img-fluid" />
-                        <img src="build/images/women_kirtan_layout.png" class="img-fluid" />
+                        <div class="row">
+                            <div class="col-sm-12 gurbani-font-2">
+                                <p class="justify-content-between font-size-20">
+                                    hwrmonIAm kIrqn iv~c sB qoN v~D vriqAw jwx vwlw Aqy kwPI h~d q~k mddgwr swj hY[ ies dI shwieqw nwl kIrqn krn (gwien) iv~c bhuq AswnI huMdI hY, gwien dIAW muFlIAW AiBAwsW nMU krn leI bhuq lwhyvMd swbq hoieAw hY Aqy ies smyN dOrwn sB qoN v~D vrqoN iv~c Awaux vwlw swj hY[
+                                </p>
+
+                                <h2 class="text-center">purSwN leI skyl</h2>
+                                <img src="build/images/men_kirtan_layout.png" class="img-fluid" />
+
+                                <h2 class="text-center">bIbIAwN leI skyl</h2>
+                                <img src="build/images/women_kirtan_layout.png" class="img-fluid" />
+                            </div>
+                        </div>
                     </div>
                     <div class="tab-pane fade" id="v-pills-sargams" role="tabpanel" aria-labelledby="v-pills-sargams-tab">
-                        Comming soon...
+                        <div class="row">
+                            <div class="col-sm-12 gurbani-font-2">
+                                <h2>1. srgm</h2>
+
+                                <p class="justify-content-between font-size-20">
+                                    sw ry gw mw pw Dw nI sW
+                                </p>
+                                <p class="justify-content-between font-size-20">
+                                    sW nI Dw pw mw gw ry sw
+                                </p>
+
+                            <p class="justify-content-between font-size-20">
+                                ies srgm dw AiBAws bhuq PwiedymMd huMdw hY[
+                            </p>
+
+                            <p class="justify-content-between font-size-20">
+                                ies dw AiBAws qyjI nwl nhIN sgoN Tihrwau nwl krnw hY, sur nUM sux sux ky[
+                            </p>
+
+                            <h2>2. srgm</h2>
+                            <p class="justify-content-between font-size-20">swsw ryry gwgw mwmw pwpw DwDw nInI sWsW</p>
+                            <p class="justify-content-between font-size-20">sWsW nInI pwpw DwDw pwpw mwmw gwgw ryry swsw[</p>
+
+                            <h2>3. srgm</h2>
+                            <p class="justify-content-between font-size-20">swswsw ryryry gwgwgw mwmwmw pwpwpw DwDwDw nInInI sWsWsW</p>
+                            <p class="justify-content-between font-size-20">sWsWsW nInInI DwDwDw pwpwpw mwmwmw gwgwgw ryryry swswsw[</p>
+
+                            <h2>4. srgm</h2>
+                            <p class="justify-content-between font-size-20">swswswsw ryrryyry gwgwgwgw mwmwmwmw pwpwpwpw DwDwDwDw nInInInI sWsWsWsW</p>
+                            <p class="justify-content-between font-size-20">sWsWsWsW nInInInI DwDwDwDw pwpwpwpw mwmwmwmw gwgwgwgw ryryryry swswswsw[</p>
                     </div>
                 </div>
             </div>
